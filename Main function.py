c16efa330c6ed3ab3da0f0f43d0f8c592ab7f5a0
yung0705 <yungchneg0705@gmail.com>
2021-10-08 01:53:58 +0800

    main function

---
+++ Main function.py
@@ -0,0 +1,14 @@
+#The Main function edited by Group leader
+print('This is ENG1003'' Week 1 Tutorial Programming Task')
+inp = input('Enter the function number to be executed: ')   #Ask for an integer
+
+if inp == '1':
+    function1()
+elif inp == '2':
+    function2()
+elif inp == '3':
+    function3()
+elif inp == '4':
+    function4()
+else:
+    print('There is no function. Please enter(1-4)')
